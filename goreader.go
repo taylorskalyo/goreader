84a66d84ec50f94635d3631e77a070cbf8f11f74
taylorskalyo <taylorskalyo@gmail.com>
2017-12-17 13:00:39 -0500

    Use termbox for UI

--- goreader.go
+++ goreader.go
@@ -5,30 +5,15 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/gdamore/tcell"
-	"github.com/gdamore/tcell/views"
+	termbox "github.com/nsf/termbox-go"
 	"github.com/taylorskalyo/goreader/epub"
 )
 
-type boxL struct {
-	views.BoxLayout
-}
-
-var (
-	box  = &boxL{}
-	app  = &views.Application{}
-	book *epub.ReadCloser
-)
-
-func (m *boxL) HandleEvent(ev tcell.Event) bool {
-	switch ev := ev.(type) {
-	case *tcell.EventKey:
-		if ev.Key() == tcell.KeyEscape {
-			app.Quit()
-			return true
-		}
-	}
-	return m.BoxLayout.HandleEvent(ev)
+type pager struct {
+	scrollX int
+	scrollY int
+	cb      cellbuf
+	chapter int
 }
 
 func main() {
@@ -37,7 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	book, err := epub.OpenReader(os.Args[1])
+	rc, err := epub.OpenReader(os.Args[1])
 	if err != nil {
 		var msg string
 		switch err {
@@ -49,37 +34,120 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Unable to open epub: %s\n", msg)
 		os.Exit(1)
 	}
-	defer book.Close()
+	defer rc.Close()
+	book := rc.Rootfiles[0]
 
-	title := views.NewTextBar()
-	title.SetStyle(tcell.StyleDefault.
-		Foreground(tcell.ColorWhite).
-		Background(tcell.ColorBlack))
-	title.SetCenter(book.Rootfiles[0].Title, tcell.StyleDefault)
-	page := views.NewTextArea()
-
-	f, err := book.Rootfiles[0].Spine.Itemrefs[0].Open()
+	f, err := book.Spine.Itemrefs[0].Open()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
-	text, err := parseText(f)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+	if err := termbox.Init(); err != nil {
 		os.Exit(1)
 	}
-	page.SetContent(text.String())
-	page.SetStyle(tcell.StyleDefault.
-		Foreground(tcell.ColorBlack).
-		Background(tcell.ColorWhite))
-
-	box.SetOrientation(views.Vertical)
-	box.AddWidget(title, 0)
-	box.AddWidget(page, 1)
-
-	app.SetRootWidget(box)
-	if err := app.Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+	cb, err := parseText(f)
+	if err != nil {
 		os.Exit(1)
+	}
+	p := pager{cb: cb, scrollX: 0, scrollY: 0}
+	if err := p.run(book); err != nil {
+		os.Exit(1)
+	}
+	termbox.Flush()
+	termbox.Close()
+}
+
+func (p pager) copyToTerm() {
+	_, height := termbox.Size()
+	for y := 0; y < height; y++ {
+		for x := 0; x < p.cb.width; x++ {
+			index := (y+p.scrollY)*p.cb.width + x
+			if index >= len(p.cb.cells) || index <= 0 {
+				continue
+			}
+			cell := p.cb.cells[index]
+			termbox.SetCell(x+p.scrollX, y, cell.Ch, cell.Fg, cell.Bg)
+		}
+	}
+}
+
+func (p *pager) draw() error {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	p.copyToTerm()
+	return termbox.Flush()
+}
+
+func (p *pager) run(book *epub.Rootfile) error {
+	for {
+		if err := p.draw(); err != nil {
+			return err
+		}
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			_, height := termbox.Size()
+			switch ev.Key {
+			case termbox.KeyEsc:
+				return nil
+			default:
+				switch ev.Ch {
+				case 'q':
+					return nil
+				case 'j':
+					if p.scrollY < len(p.cb.cells)/p.cb.width-height {
+						p.scrollY++
+					}
+				case 'k':
+					if p.scrollY > 0 {
+						p.scrollY--
+					}
+				case 'h':
+					if p.scrollX > -p.cb.width {
+						p.scrollX--
+					}
+				case 'l':
+					if p.scrollX < 0 {
+						p.scrollX++
+					}
+				case 'f':
+					if p.scrollY < len(p.cb.cells)/p.cb.width-height {
+						p.scrollY += height
+					}
+				case 'b':
+					if p.scrollY > 0 {
+						p.scrollY -= height
+					}
+				case 'L':
+					if p.chapter < len(book.Spine.Itemrefs)-1 {
+						p.scrollY = 0
+						p.scrollX = 0
+						p.chapter++
+						f, err := book.Spine.Itemrefs[p.chapter].Open()
+						if err != nil {
+							return err
+						}
+						cb, err := parseText(f)
+						if err != nil {
+							return err
+						}
+						p.cb = cb
+					}
+				case 'H':
+					if p.chapter > 0 {
+						p.scrollY = 0
+						p.scrollX = 0
+						p.chapter--
+						f, err := book.Spine.Itemrefs[p.chapter].Open()
+						if err != nil {
+							return err
+						}
+						cb, err := parseText(f)
+						if err != nil {
+							return err
+						}
+						p.cb = cb
+					}
+				}
+			}
+		}
 	}
 }
--- parse.go
+++ parse.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
+	"strings"
+	"unicode/utf8"
+
+	termbox "github.com/nsf/termbox-go"
 
 	"golang.org/x/net/html"
 )
@@ -12,18 +17,101 @@ type parser struct {
 	elStack   []string
 	tokenizer *html.Tokenizer
 	buf       bytes.Buffer
+	cbuf      cellbuf
+}
+
+type cellbuf struct {
+	cells   []termbox.Cell
+	width   int
+	lmargin int
+	col     int
+	row     int
+}
+
+// setCell changes a cell's attributes in the cellbuf at the given position.
+func (c *cellbuf) setCell(x, y int, ch rune, fg, bg termbox.Attribute) {
+	// Grow by 1024 when out of space
+	for y*c.width+x >= len(c.cells) {
+		c.cells = append(c.cells, make([]termbox.Cell, 1024)...)
+	}
+	c.cells[y*c.width+x] = termbox.Cell{Ch: ch, Fg: fg, Bg: bg}
+}
+
+// scanWords is a split function for a Scanner that returns space-separated
+// words.
+func scanWords(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if r != ' ' {
+			break
+		}
+	}
+
+	// Scan until space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if r == ' ' {
+			return i + width, data[start:i], nil
+		}
+	}
+
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+
+	// Request more data.
+	return start, nil, nil
+}
+
+// appendText appends styled text to the cellbuf.
+func (c *cellbuf) appendStyledText(str string, fg, bg termbox.Attribute) {
+	if c.col < c.lmargin {
+		c.col = c.lmargin
+	}
+	scanner := bufio.NewScanner(strings.NewReader(str))
+	scanner.Split(scanWords)
+	for scanner.Scan() {
+		word := []rune(scanner.Text())
+		if len(word) > c.width-c.col {
+			c.row++
+			c.col = c.lmargin
+		}
+		for _, r := range word {
+			if r == '\n' {
+				c.row++
+				c.col = c.lmargin
+				continue
+			}
+			c.setCell(c.col, c.row, r, fg, bg)
+			c.col++
+		}
+		if c.col < c.width {
+			c.col++
+		}
+	}
+}
+
+// appendText appends text to the cellbuf.
+func (c *cellbuf) appendText(str string) {
+	c.appendStyledText(str, termbox.ColorDefault, termbox.ColorDefault)
 }
 
 // parseText takes in html content via an io.Reader and returns a buffer
 // containing only plain text.
-func parseText(r io.Reader) (bytes.Buffer, error) {
+func parseText(r io.Reader) (cellbuf, error) {
 	tokenizer := html.NewTokenizer(r)
-	p := parser{tokenizer: tokenizer}
+	cbuf := cellbuf{width: 80}
+	p := parser{tokenizer: tokenizer, cbuf: cbuf}
 	err := p.parse(r)
 	if err != nil {
-		return p.buf, err
+		return p.cbuf, err
 	}
-	return p.buf, nil
+	return p.cbuf, nil
 }
 
 // parse walks an html document and appends text elements to a buffer.
@@ -36,9 +124,9 @@ func (p *parser) parse(io.Reader) (err error) {
 			p.elStack = append(p.elStack, p.tokenizer.Token().Data) // push element
 			fallthrough
 		case html.SelfClosingTagToken:
-			err = p.handleTags()
+			p.handleTags()
 		case html.TextToken:
-			err = p.handleText()
+			p.handleText()
 		case html.EndTagToken:
 			p.elStack = p.elStack[:len(p.elStack)-1] // pop element
 		}
@@ -52,30 +140,29 @@ func (p *parser) parse(io.Reader) (err error) {
 
 // handleText appends text elements to the parser buffer. It filters elements
 // that should not be displayed as text (e.g. style blocks).
-func (p *parser) handleText() error {
+func (p *parser) handleText() {
 	// Skip style tags
 	if len(p.elStack) > 0 && p.elStack[len(p.elStack)-1] == "style" {
-		return nil
+		return
 	}
-	_, err := p.buf.Write(p.tokenizer.Text())
-	return err
+	p.cbuf.appendText(string(p.tokenizer.Text()))
 }
 
 // handleTags appends text representations of non-text elements (e.g. image alt
 // tags) to the parser buffer.
-func (p *parser) handleTags() error {
+func (p *parser) handleTags() {
 	token := p.tokenizer.Token()
 	// Display alt text in place of image
-	if token.Data == "img" {
+	switch token.Data {
+	case "img":
 		for _, a := range token.Attr {
-			if a.Key == "alt" {
-				_, err := p.buf.Write([]byte(fmt.Sprintf("\nImage alt text: %s\n", a.Val)))
-				if err != nil {
-					return err
-				}
+			switch a.Key {
+			case "alt":
+				text := fmt.Sprintf("\nImage alt text: %s\n", a.Val)
+				p.cbuf.appendText(text)
 			}
 		}
+	case "br":
+		p.cbuf.appendText("\n")
 	}
-
-	return nil
 }
