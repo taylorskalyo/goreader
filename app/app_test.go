8b2404ba751ed6186015a0fbb55c47ff678267f6
taylorskalyo <taylorskalyo@users.noreply.github.com>
2023-06-22 19:19:02 -0400

    style: refactor verifyMethodCall func
    - Removes hard-coded termbox.KeyArrowUp value
    - Removes unicode.IsLetter check
    - Prevents panic on unrecognized input

--- app/app_test.go
+++ app/app_test.go
@@ -2,9 +2,9 @@ package app
 
 import (
 	"testing"
-	"unicode"
 
 	termbox "github.com/nsf/termbox-go"
+	"github.com/stretchr/testify/mock"
 	localMock "github.com/taylorskalyo/goreader/mock"
 )
 
@@ -16,34 +16,35 @@ func TestInitNavigationKeys(t *testing.T) {
 
 	keymap, chmap := initNavigationKeys(a)
 
-	var zeroValueTermboxKey termbox.Key
+	verifyMethodCall := func(receiver *mock.Mock, methodName string, input any) {
+		receiver.On(methodName).Return()
 
-	verifyMethodCall := func(receiver string, methodName string, letterKey rune, tbKey termbox.Key) {
-		if receiver == "pager" {
-			p.On(methodName).Return()
-		}
-		if receiver == "app" {
-			a.On(methodName).Return()
-		}
-
-		if unicode.IsLetter(letterKey) {
-			chmap[letterKey]()
-		}
-		if tbKey != zeroValueTermboxKey {
-			keymap[termbox.KeyArrowUp]()
+		if ch, ok := input.(rune); ok {
+			chmap[ch]()
+		} else if key, ok := input.(termbox.Key); ok {
+			keymap[key]()
+		} else {
+			t.Errorf("unhandled input: %+v", input)
 		}
 
 		p.AssertExpectations(t)
 	}
-	verifyMethodCall("pager", "ScrollUp", 'k', termbox.KeyArrowUp)
-	verifyMethodCall("pager", "ScrollDown", 'j', termbox.KeyArrowDown)
-	verifyMethodCall("pager", "ScrollLeft", 'h', termbox.KeyArrowLeft)
-	verifyMethodCall("pager", "ScrollRight", 'l', termbox.KeyArrowRight)
-	verifyMethodCall("pager", "ToTop", 'g', zeroValueTermboxKey)
-	verifyMethodCall("pager", "ToBottom", 'G', zeroValueTermboxKey)
-	verifyMethodCall("app", "Forward", 'f', zeroValueTermboxKey)
-	verifyMethodCall("app", "Back", 'b', zeroValueTermboxKey)
-	verifyMethodCall("app", "NextChapter", 'L', zeroValueTermboxKey)
-	verifyMethodCall("app", "PrevChapter", 'H', zeroValueTermboxKey)
-	verifyMethodCall("app", "Exit", 'q', termbox.KeyEsc)
+
+	verifyMethodCall(&p.Mock, "ScrollUp", termbox.KeyArrowUp)
+	verifyMethodCall(&p.Mock, "ScrollUp", 'k')
+	verifyMethodCall(&p.Mock, "ScrollDown", termbox.KeyArrowDown)
+	verifyMethodCall(&p.Mock, "ScrollDown", 'j')
+	verifyMethodCall(&p.Mock, "ScrollLeft", termbox.KeyArrowLeft)
+	verifyMethodCall(&p.Mock, "ScrollLeft", 'h')
+	verifyMethodCall(&p.Mock, "ScrollRight", termbox.KeyArrowRight)
+	verifyMethodCall(&p.Mock, "ScrollRight", 'l')
+	verifyMethodCall(&p.Mock, "ToTop", 'g')
+	verifyMethodCall(&p.Mock, "ToBottom", 'G')
+
+	verifyMethodCall(&a.Mock, "Exit", termbox.KeyEsc)
+	verifyMethodCall(&a.Mock, "Exit", 'q')
+	verifyMethodCall(&a.Mock, "Forward", 'f')
+	verifyMethodCall(&a.Mock, "Back", 'b')
+	verifyMethodCall(&a.Mock, "NextChapter", 'L')
+	verifyMethodCall(&a.Mock, "PrevChapter", 'H')
 }
